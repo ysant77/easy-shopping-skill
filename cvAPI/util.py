b2fa77dde8a06e74b9ff840d9ced796f9d87d9b4
ysant77 <ysant77@gmail.com>
2024-01-15 08:46:21 +0800

    removed google api  key

--- cvAPI/util.py
+++ cvAPI/util.py
@@ -3,7 +3,7 @@ from requests import Session
 import io
 import base64
 
-API_KEY = 'AIzaSyD13Z9EzrTj6bna7_seIV0cTlgGEDOnXGg'
+API_KEY = 'api_key'
 
 def encode_image_from_file(file_name):
     with io.open(file_name, 'rb') as image_file:
